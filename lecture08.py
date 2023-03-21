b39b5fd7c4f522f42e0a780d8cf08f1b96229c69
NithanB <pa.nithan_st@tni.ac.th>
2023-03-22 00:37:35 +0700

    Update lecture08.py

--- lecture08.py
+++ lecture08.py
@@ -1,5 +1,5 @@
 import sympy
-from sympy import symbols, integrate, limit
+from sympy import symbols, integrate, limit, cos, sin, exp
 x = symbols('x')
 
 
@@ -16,3 +16,32 @@ print("∫f(x)dx = {}".format(y_integral))
 
 print("limf(x) x -> 0 = {}".format(y_limit))
 
+y2 = (2 + cos(x))/(sin(x))**2
+y_diff_2 = y2.diff(x)
+y_integral_2 = integrate(y2,x)
+y_limit_2 = limit(y2, x, 0)
+
+print()
+
+print("f(x) = {}".format(y2))
+
+print("f'(x) = {}".format(y_diff_2))
+
+print("∫f(x)dx = {}".format(y_integral_2))
+
+print("limf(x) x -> 0 = {}".format(y_limit_2))
+
+y3 = x**3 * exp(-4*x)
+y_diff_3 = y3.diff(x)
+y_integral_3 = integrate(y3,x)
+y_limit_3 = limit(y3, x, 0)
+
+print()
+
+print("f(x) = {}".format(y3))
+
+print("f'(x) = {}".format(y_diff_3))
+
+print("∫f(x)dx = {}".format(y_integral_3))
+
+print("limf(x) x -> 0 = {}".format(y_limit_3))
